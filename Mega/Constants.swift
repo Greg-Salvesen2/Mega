65f5e2f38a8dd6f52e5af473d6c759b1f022e1fa
Greg Salvesen <greg@latenightdesigns.com>
2016-03-17 10:50:47 -0400

    First Commit

---
+++ Mega/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Mega
+//
+//  Created by Greg Salvesen on 3/17/16.
+//  Copyright © 2016 Mega. All rights reserved.
+//
+
+import Foundation
+
+let kURLHeader = "http://www.latenightdesigns.com/mega/"
+let kURLLogin = "login.php"
---
+++ Mega/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Mega
+//
+//  Created by Greg Salvesen on 3/8/16.
+//  Copyright © 2016 Mega. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+
+
+}
+
